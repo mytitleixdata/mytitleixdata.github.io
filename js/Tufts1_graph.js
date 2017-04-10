1ea1729ab31fd77a183526f20556b00eee661049
Rachel Hanebutt <rachelhanebutt@gmail.com>
2017-04-10 18:07:28 -0400

    no message

---
+++ js/Tufts1_graph.js
@@ -0,0 +1,139 @@
+$(function(){
+
+console.log('is this happening?');
+
+    if($('#Tufts').css('display') == 'block') {
+
+        $('#Tufts1').highcharts({
+            chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Tufts University Clery Act Report Data'
+    },
+    subtitle: {
+        text: 'Sexual Assault Reports from the Campus Safety and Security Data Tool<a href="https://www.bostonglobe.com/2016/12/23/table-reports-sexual-assault-new-england-colleges/BG53TpCamCzxeCUF7tixYN/story.html</a>.'
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: 'Number of Reports'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: false,
+                format: ''
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> Reports<br/>'
+    },
+
+    series: [{
+        name: 'Tufts University Reports',
+        colorByPoint: true,
+        data: [{
+            name: '2011',
+            y: 6,
+            drilldown: '2011'
+        }, {
+            name: '2012',
+            y: 6,
+            drilldown: '2012'
+        }, {
+            name: '2013',
+            y: 13,
+            drilldown: '2013'
+        }, {
+            name: '2014',
+            y: 14,
+            drilldown: '2014'
+        }, {
+            name: '2015',
+            y: 25,
+            drilldown: '2015'
+        }]
+    }],
+            drilldown: {
+                series: [{
+                    name: 'Actual Reports vs. Expected Reports, 2011',
+                    id: '2011',
+                    data: [
+                        [
+                            'Actual Reports',
+                            6
+                        ],
+                        [
+                            'Expected Reports',
+                            2174.4
+                        ]
+                    ]
+              }, {
+                    name: 'Actual Reports vs. Expected Reports, 2012',
+                    id: '2012',
+                    data: [
+                        [
+                            'Actual Reports',
+                            6
+                        ],
+                        [
+                            'Expected Reports',
+                            2174.4
+                        ]
+                    ]
+                    }, {
+                    name: 'Actual Reports vs. Expected Reports, 2013',
+                    id: '2013',
+                    data: [
+                        [
+                            'Actual Reports',
+                            13
+                        ],
+                        [
+                            'Expected Reports',
+                            2174.4
+                        ]
+                    ]
+                    }, {
+                    name: 'Actual Reports vs. Expected Reports, 2014',
+                    id: '2014',
+                    data: [
+                        [
+                            'Actual Reports',
+                            14
+                        ],
+                        [
+                            'Expected Reports',
+                            2174.4
+                        ]
+                    ]
+                    }, {
+                    name: 'Actual Reports vs. Expected Reports, 2015',
+                    id: '2015',
+                    data: [
+                        [
+                            'Actual Reports',
+                            25
+                        ],
+                        [
+                            'Expected Reports',
+                            2174.4
+                        ]
+                    ]
+                }]
+            }
+            });
+                       
+    }
+});
---
+++ js/Yale.js
@@ -0,0 +1,139 @@
+$(function(){
+
+console.log('is this happening?');
+
+    if($('#Yale').css('display') == 'block') {
+
+        $('#Yale1').highcharts({
+            chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Yale University Clery Act Report Data'
+    },
+    subtitle: {
+        text: 'Sexual Assault Reports from the Campus Safety and Security Data Tool<a href="https://www.bostonglobe.com/2016/12/23/table-reports-sexual-assault-new-england-colleges/BG53TpCamCzxeCUF7tixYN/story.html</a>.'
+    },
+    xAxis: {
+        type: 'category'
+    },
+    yAxis: {
+        title: {
+            text: 'Number of Reports'
+        }
+    },
+    legend: {
+        enabled: false
+    },
+    plotOptions: {
+        series: {
+            borderWidth: 0,
+            dataLabels: {
+                enabled: false,
+                format: ''
+            }
+        }
+    },
+
+    tooltip: {
+        headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+        pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y}</b> Reports<br/>'
+    },
+
+    series: [{
+        name: 'Yale University Reports',
+        colorByPoint: true,
+        data: [{
+            name: '2011',
+            y: 20,
+            drilldown: '2011'
+        }, {
+            name: '2012',
+            y: 16,
+            drilldown: '2012'
+        }, {
+            name: '2013',
+            y: 12,
+            drilldown: '2013'
+        }, {
+            name: '2014',
+            y: 18,
+            drilldown: '2014'
+        }, {
+            name: '2015',
+            y: 30,
+            drilldown: '2015'
+        }]
+    }],
+            drilldown: {
+                series: [{
+                    name: 'Actual Reports vs. Expected Reports, 2011',
+                    id: '2011',
+                    data: [
+                        [
+                            'Actual Reports',
+                            20
+                        ],
+                        [
+                            'Expected Reports',
+                            2477
+                        ]
+                    ]
+              }, {
+                    name: 'Actual Reports vs. Expected Reports, 2012',
+                    id: '2012',
+                    data: [
+                        [
+                            'Actual Reports',
+                            16
+                        ],
+                        [
+                            'Expected Reports',
+                            2477
+                        ]
+                    ]
+                    }, {
+                    name: 'Actual Reports vs. Expected Reports, 2013',
+                    id: '2013',
+                    data: [
+                        [
+                            'Actual Reports',
+                            12
+                        ],
+                        [
+                            'Expected Reports',
+                            2477
+                        ]
+                    ]
+                    }, {
+                    name: 'Actual Reports vs. Expected Reports, 2014',
+                    id: '2014',
+                    data: [
+                        [
+                            'Actual Reports',
+                            18
+                        ],
+                        [
+                            'Expected Reports',
+                            2477
+                        ]
+                    ]
+                    }, {
+                    name: 'Actual Reports vs. Expected Reports, 2015',
+                    id: '2015',
+                    data: [
+                        [
+                            'Actual Reports',
+                            30
+                        ],
+                        [
+                            'Expected Reports',
+                            2477
+                        ]
+                    ]
+                }]
+            }
+            });
+                       
+    }
+});
